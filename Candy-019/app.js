05b81d14280472e9400f84e24754cea0fbe109c3
Ma <z2715674@gmail.com>
2024-12-18 23:31:26 +0800

    add Candy-019

---
+++ Candy-019/app.js
@@ -0,0 +1,17 @@
+// 編號：CANDY-019
+// 程式語言：JavaScript
+// 題目：檢查是否為某個數字的平方數
+
+function isSquare(num) {
+  const sqrtNum = Math.sqrt(num);
+  return Math.floor(sqrtNum) == sqrtNum;
+}
+// Math.sqrt() 會回傳數字的平方根
+//使用 Math.floor() 取整數判斷平方根是否也為整數
+
+console.log(isSquare(0)); // true
+console.log(isSquare(4)); // true
+console.log(isSquare(5)); // false
+console.log(isSquare(100)); // true
+console.log(isSquare(-4)); // false
+console.log(isSquare(-1)); // false
