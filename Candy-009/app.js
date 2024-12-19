c4896034f6134403f690eff59c4790fcd69b5d48
Ma <z2715674@gmail.com>
2024-12-19 23:25:47 +0800

    add Candy-009

--- Candy-009/app.js
+++ Candy-009/app.js
@@ -6,7 +6,7 @@ function removeAnchor(url) {
   return url.split("#")[0];
 }
 // 使用 split() 將 # 作為切割點, 第二個值為返回值
-// 由於已經被 # 分割成兩半, 一半為#之前, 另一半為 # 之後
+// 由於已經被 "#"" 分割成兩半, 一半為#之前, 另一半為 # 之後
 // 利用索引值抓取前面的值並回傳
 
 console.log(removeAnchor("5xcampus.com")); // 印出 5xcampus.com
